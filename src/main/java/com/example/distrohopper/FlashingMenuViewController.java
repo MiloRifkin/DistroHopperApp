cab4871ba8710f9418d9b6175079b3286fd2841b
milor <milo.rifkin1@gmail.com>
2025-04-28 00:49:12 +0100

    Updated- flashing now working progress bars now working

--- src/main/java/com/example/distrohopper/FlashingMenuViewController.java
+++ src/main/java/com/example/distrohopper/FlashingMenuViewController.java
@@ -13,9 +13,11 @@ import javafx.scene.text.Font;
 import javafx.concurrent.Task;
 import javafx.stage.Stage;
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 //endregion
 
 public class FlashingMenuViewController {
@@ -75,6 +77,8 @@ public class FlashingMenuViewController {
     public Label arrowLabel;
     public Label errorLabel;
     public Label flashingComplete;
+    public Label downloading;
+    public Label flashing;
 
     //endRegion
 
@@ -92,10 +96,10 @@ public class FlashingMenuViewController {
      * @param progressBar Used to update the download progress
      * @return void
      */
-    private Task<Void> createDownloadTask(ProgressBar progressBar) {
+    private Task<Void> createDownloadTask(ProgressBar progressBar, Label downloading) {
 
 
-        return new Task<>() {
+        Task<Void> ISODownload = new Task<>(){
             @Override
             public Void call() throws Exception {
 
@@ -118,9 +122,9 @@ public class FlashingMenuViewController {
                         }
 
                         updateProgress(100,100);
-
                         Task<Void> flashingTask = createFlashingTask(flashingProgressBar);
                         Thread flashingThread = new Thread(flashingTask);
+                        downloading.setText("Download Complete");
                         System.out.println("Download Complete");
                         flashingThread.start();
                     }
@@ -128,13 +132,17 @@ public class FlashingMenuViewController {
                 return null;
             }
         };
+        progressBar.progressProperty().bind(ISODownload.progressProperty());
+
+        return ISODownload;
     }
 
     private Task<Void> createFlashingTask(ProgressBar flashingProgressBar){
-        return new Task<Void>() {
+        Task<Void> flashingTask = new Task<Void>() {
             @Override
             public Void call() throws Exception{
-
+                File file = new File(driveLetter + ":\\");
+                int loopCounter = 0;
                 //System runs code which emulates following PowerShell call: .\diskMount.ps1 Y X Fedora_1.4 1
                 System.out.println("Building process:");
                 ProcessBuilder builder = new ProcessBuilder(
@@ -151,18 +159,26 @@ public class FlashingMenuViewController {
                 );
                 try {
                     Process p = builder.start();
+                    TimeUnit.SECONDS.sleep(15);
+                    double totalSpace = (double) file.getUsableSpace() / (1024 * 1024 * 1024) - 0.000000001;
+                    double freeSpace = (double) file.getUsableSpace() / (1024 * 1024 * 1024) - 0.000000001;
+                    while(loopCounter < 9){
+                        if (freeSpace ==((double) file.getUsableSpace() / (1024 * 1024 * 1024))){ loopCounter++; }
+                        freeSpace = (double) file.getUsableSpace() / (1024*1024*1024);
+                        updateProgress((totalSpace-freeSpace), 1);
+                        TimeUnit.SECONDS.sleep(1);
+                    }
                     int result = p.waitFor();
+                    updateProgress(100,100);
+                    flashing.setText("Flashing Complete!");
                     System.out.println("Exited with code: " + result);
                 } catch (IOException | InterruptedException e) {e.printStackTrace();}
-//                ISOFlasher flasher = new ISOFlasher();
-//                if (flasher.flashISO(unusedDriveLetter, driveLetter, distroVersion, driveNumber)){
-//                    flashingComplete.setText("Flashing complete!");
-//                    System.out.println("Complete!");
-//                }
                 return null;
             }
 
         };
+        flashingProgressBar.progressProperty().bind(flashingTask.progressProperty());
+        return flashingTask;
     }
 
 
@@ -178,10 +194,9 @@ public class FlashingMenuViewController {
         arrowLabel.setFont(new Font(40));
         leftLabel.setText(distroName);
         rightLabel.setText("USB Drive");
-        Task<Void> downloadTask = createDownloadTask(progressBar);
+        Task<Void> downloadTask = createDownloadTask(progressBar, downloading);
         Thread downloadThread = new Thread(downloadTask);
         downloadThread.start();
-
     }
 
 
