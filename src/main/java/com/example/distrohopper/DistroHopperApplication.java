d279ac2a16000ddb002a5fd60094bd86fc8eba32
Dominic <DominicHSC@protonmail.com>
2025-04-28 17:49:50 +0100

    Add Icon

--- src/main/java/com/example/distrohopper/DistroHopperApplication.java
+++ src/main/java/com/example/distrohopper/DistroHopperApplication.java
@@ -5,6 +5,7 @@ import atlantafx.base.theme.PrimerLight;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 //endregion
 
@@ -21,6 +22,7 @@ public class DistroHopperApplication extends Application {
             stage.setWidth(640);
             stage.setHeight(640);
             stage.setResizable(false);
+            stage.getIcons().add(new Image("icon.jpeg"));
             stage.show();
 
         }catch(Exception E){
